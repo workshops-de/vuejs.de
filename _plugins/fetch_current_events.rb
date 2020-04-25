ff1d69020aa804285e4af15b3de114823caa5e5e
Robin Böhm <robin.boehm@workshops.de>
2020-04-25 19:51:54 +0200

    chore(user): remove old users

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -1,17 +1,5 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
   response = RestClient.get('https://workshops.de/api/course/4/events')
-  File.write('_data/events/angular-intensiv.json', response.body)
-
-  response = RestClient.get('https://workshops.de/api/course/23/events')
-  File.write('_data/events/angular-enterprise-applications.json', response.body)
-
-  response = RestClient.get('https://workshops.de/api/course/29/events')
-  File.write('_data/events/nestjs.json', response.body)
-
-  response = RestClient.get('https://workshops.de/api/course/28/events')
-  File.write('_data/events/rxjs.json', response.body)
-
-  response = RestClient.get('https://workshops.de/api/course/4/events')
-  File.write('_data/events/angular-remote.json', response.body)
+  File.write('_data/events/vuejs-intensiv.json', response.body)
 end
