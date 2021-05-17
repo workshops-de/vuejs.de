a085254cfba20620815c3abbb37756a4bfc9a821
Antony Konstantinidis <ak@digitalessence.de>
2021-05-17 11:36:18 +0200

    added additional workshops

--- _plugins/fetch_current_events.rb
+++ _plugins/fetch_current_events.rb
@@ -1,5 +1,11 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'rest-client'
   response = RestClient.get('https://workshops.de/api/course/37/events')
-  File.write('_data/events/vuejs-intensiv.json', response.body)
+  File.write('_data/events/vuejs-typescript.json', response.body)
+
+  response = RestClient.get('https://workshops.de/api/course/44/events')
+  File.write('_data/events/vuejs-javascript.json', response.body)
+
+  response = RestClient.get('https://workshops.de/api/course/45/events')
+  File.write('_data/events/vuejs-composition-api.json', response.body)
 end
